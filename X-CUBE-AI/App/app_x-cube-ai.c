e506373ecae2ed830bd7716ba98151544fada541
Lingjia007 <3125497423@qq.com>
2026-07-15 02:54:29 +0800

    ccm

--- X-CUBE-AI/App/app_x-cube-ai.c
+++ X-CUBE-AI/App/app_x-cube-ai.c
@@ -1,48 +1,47 @@
 
 /**
- ******************************************************************************
- * @file    app_x-cube-ai.c
- * @author  X-CUBE-AI C code generator
- * @brief   AI program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2026 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file    app_x-cube-ai.c
+  * @author  X-CUBE-AI C code generator
+  * @brief   AI program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
 
-/*
- * Description
- *   v1.0 - Minimum template to show how to use the Embedded Client API
- *          model. Only one input and one output is supported. All
- *          memory resources are allocated statically (AI_NETWORK_XX, defines
- *          are used).
- *          Re-target of the printf function is out-of-scope.
- *   v2.0 - add multiple IO and/or multiple heap support
- *
- *   For more information, see the embeded documentation:
- *
- *       [1] %X_CUBE_AI_DIR%/Documentation/index.html
- *
- *   X_CUBE_AI_DIR indicates the location where the X-CUBE-AI pack is installed
- *   typical : C:\Users\[user_name]\STM32Cube\Repository\STMicroelectronics\X-CUBE-AI\7.1.0
- */
+ /*
+  * Description
+  *   v1.0 - Minimum template to show how to use the Embedded Client API
+  *          model. Only one input and one output is supported. All
+  *          memory resources are allocated statically (AI_NETWORK_XX, defines
+  *          are used).
+  *          Re-target of the printf function is out-of-scope.
+  *   v2.0 - add multiple IO and/or multiple heap support
+  *
+  *   For more information, see the embeded documentation:
+  *
+  *       [1] %X_CUBE_AI_DIR%/Documentation/index.html
+  *
+  *   X_CUBE_AI_DIR indicates the location where the X-CUBE-AI pack is installed
+  *   typical : C:\Users\[user_name]\STM32Cube\Repository\STMicroelectronics\X-CUBE-AI\7.1.0
+  */
 
 #ifdef __cplusplus
-extern "C"
-{
+ extern "C" {
 #endif
 
-  /* Includes ------------------------------------------------------------------*/
+/* Includes ------------------------------------------------------------------*/
 
-#if defined(__ICCARM__)
-#elif defined(__CC_ARM) || (__GNUC__)
+#if defined ( __ICCARM__ )
+#elif defined ( __CC_ARM ) || ( __GNUC__ )
 #endif
 
 /* System headers */
@@ -73,47 +72,49 @@ extern "C"
   extern const int16_t g_up_1000ms_audio_data[];
   extern const int16_t g_down_1000ms_audio_data[];
   extern const int16_t g_left_1000ms_audio_data[];
-  /* USER CODE END includes */
+/* USER CODE END includes */
 
-  /* IO buffers ----------------------------------------------------------------*/
+/* IO buffers ----------------------------------------------------------------*/
 
 #if !defined(AI_MICRO_SPEECH_INPUTS_IN_ACTIVATIONS)
-  AI_ALIGNED(4)
-  ai_i8 data_in_1[AI_MICRO_SPEECH_IN_1_SIZE_BYTES];
-  ai_i8 *data_ins[AI_MICRO_SPEECH_IN_NUM] = {
-      data_in_1};
+AI_ALIGNED(4) ai_i8 data_in_1[AI_MICRO_SPEECH_IN_1_SIZE_BYTES];
+ai_i8* data_ins[AI_MICRO_SPEECH_IN_NUM] = {
+data_in_1
+};
 #else
-ai_i8 *data_ins[AI_MICRO_SPEECH_IN_NUM] = {
-    NULL};
+ai_i8* data_ins[AI_MICRO_SPEECH_IN_NUM] = {
+NULL
+};
 #endif
 
 #if !defined(AI_MICRO_SPEECH_OUTPUTS_IN_ACTIVATIONS)
-  AI_ALIGNED(4)
-  ai_i8 data_out_1[AI_MICRO_SPEECH_OUT_1_SIZE_BYTES];
-  ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
-      data_out_1};
+AI_ALIGNED(4) ai_i8 data_out_1[AI_MICRO_SPEECH_OUT_1_SIZE_BYTES];
+ai_i8* data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
+data_out_1
+};
 #else
-ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
-    NULL};
+ai_i8* data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
+NULL
+};
 #endif
 
-  /* Activations buffers -------------------------------------------------------*/
+/* Activations buffers -------------------------------------------------------*/
 
-  AI_ALIGNED(32)
-  static uint8_t pool0[AI_MICRO_SPEECH_DATA_ACTIVATION_1_SIZE];
+AI_ALIGNED(32)
+static uint8_t pool0[AI_MICRO_SPEECH_DATA_ACTIVATION_1_SIZE];
 
-  ai_handle data_activations0[] = {pool0};
+ai_handle data_activations0[] = {pool0};
 
-  /* AI objects ----------------------------------------------------------------*/
+/* AI objects ----------------------------------------------------------------*/
 
-  static ai_handle micro_speech = AI_HANDLE_NULL;
+static ai_handle micro_speech = AI_HANDLE_NULL;
 
-  static ai_buffer *ai_input;
-  static ai_buffer *ai_output;
+static ai_buffer* ai_input;
+static ai_buffer* ai_output;
 
-  static void ai_log_err(const ai_error err, const char *fct)
-  {
-    /* USER CODE BEGIN log */
+static void ai_log_err(const ai_error err, const char *fct)
+{
+  /* USER CODE BEGIN log */
     if (fct)
       printf("TEMPLATE - Error (%s) - type=0x%02x code=0x%02x\r\n", fct,
              err.type, err.code);
@@ -123,73 +124,67 @@ ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
     do
     {
     } while (1);
-    /* USER CODE END log */
+  /* USER CODE END log */
+}
+
+static int ai_boostrap(ai_handle *act_addr)
+{
+  ai_error err;
+
+  /* Create and initialize an instance of the model */
+  err = ai_micro_speech_create_and_init(&micro_speech, act_addr, NULL);
+  if (err.type != AI_ERROR_NONE) {
+    ai_log_err(err, "ai_micro_speech_create_and_init");
+    return -1;
   }
 
-  static int ai_boostrap(ai_handle *act_addr)
-  {
-    ai_error err;
-
-    /* Create and initialize an instance of the model */
-    err = ai_micro_speech_create_and_init(&micro_speech, act_addr, NULL);
-    if (err.type != AI_ERROR_NONE)
-    {
-      ai_log_err(err, "ai_micro_speech_create_and_init");
-      return -1;
-    }
-
-    ai_input = ai_micro_speech_inputs_get(micro_speech, NULL);
-    ai_output = ai_micro_speech_outputs_get(micro_speech, NULL);
+  ai_input = ai_micro_speech_inputs_get(micro_speech, NULL);
+  ai_output = ai_micro_speech_outputs_get(micro_speech, NULL);
 
 #if defined(AI_MICRO_SPEECH_INPUTS_IN_ACTIVATIONS)
-    /*  In the case where "--allocate-inputs" option is used, memory buffer can be
-     *  used from the activations buffer. This is not mandatory.
-     */
-    for (int idx = 0; idx < AI_MICRO_SPEECH_IN_NUM; idx++)
-    {
-      data_ins[idx] = ai_input[idx].data;
-    }
+  /*  In the case where "--allocate-inputs" option is used, memory buffer can be
+   *  used from the activations buffer. This is not mandatory.
+   */
+  for (int idx=0; idx < AI_MICRO_SPEECH_IN_NUM; idx++) {
+	data_ins[idx] = ai_input[idx].data;
+  }
 #else
-  for (int idx = 0; idx < AI_MICRO_SPEECH_IN_NUM; idx++)
-  {
-    ai_input[idx].data = data_ins[idx];
+  for (int idx=0; idx < AI_MICRO_SPEECH_IN_NUM; idx++) {
+	  ai_input[idx].data = data_ins[idx];
   }
 #endif
 
 #if defined(AI_MICRO_SPEECH_OUTPUTS_IN_ACTIVATIONS)
-    /*  In the case where "--allocate-outputs" option is used, memory buffer can be
-     *  used from the activations buffer. This is no mandatory.
-     */
-    for (int idx = 0; idx < AI_MICRO_SPEECH_OUT_NUM; idx++)
-    {
-      data_outs[idx] = ai_output[idx].data;
-    }
+  /*  In the case where "--allocate-outputs" option is used, memory buffer can be
+   *  used from the activations buffer. This is no mandatory.
+   */
+  for (int idx=0; idx < AI_MICRO_SPEECH_OUT_NUM; idx++) {
+	data_outs[idx] = ai_output[idx].data;
+  }
 #else
-  for (int idx = 0; idx < AI_MICRO_SPEECH_OUT_NUM; idx++)
-  {
-    ai_output[idx].data = data_outs[idx];
+  for (int idx=0; idx < AI_MICRO_SPEECH_OUT_NUM; idx++) {
+	ai_output[idx].data = data_outs[idx];
   }
 #endif
 
-    return 0;
+  return 0;
+}
+
+static int ai_run(void)
+{
+  ai_i32 batch;
+
+  batch = ai_micro_speech_run(micro_speech, ai_input, ai_output);
+  if (batch != 1) {
+    ai_log_err(ai_micro_speech_get_error(micro_speech),
+        "ai_micro_speech_run");
+    return -1;
   }
 
-  static int ai_run(void)
-  {
-    ai_i32 batch;
+  return 0;
+}
 
-    batch = ai_micro_speech_run(micro_speech, ai_input, ai_output);
-    if (batch != 1)
-    {
-      ai_log_err(ai_micro_speech_get_error(micro_speech),
-                 "ai_micro_speech_run");
-      return -1;
-    }
-
-    return 0;
-  }
-
-  /* USER CODE BEGIN 2 */
+/* USER CODE BEGIN 2 */
 
 /* ---- Test data table ---- */
 #define NUM_TEST_WORDS 10
@@ -230,7 +225,7 @@ ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
   /* ---- TIM5 32-bit timer for cycle-accurate timing (us) ---- */
   /* DWT CYCCNT is unreliable: audio_frontend/CMSIS-DSP internally disables it. */
   /* TIM5 is on APB1 (42MHz), timer clock = 84MHz when APB1 prescaler != 1.     */
-#define ENABLE_TIMING
+// #define ENABLE_TIMING
 #define TIM5_TIMER_CLOCK 84000000U
 
 #ifdef ENABLE_TIMING
@@ -327,12 +322,12 @@ ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
     }
     return 0;
   }
-  /* USER CODE END 2 */
+/* USER CODE END 2 */
 
-  /* Entry points --------------------------------------------------------------*/
+/* Entry points --------------------------------------------------------------*/
 
-  void MX_X_CUBE_AI_Init(void)
-  {
+void MX_X_CUBE_AI_Init(void)
+{
     /* USER CODE BEGIN 5 */
     printf("\r\n=== micro_speech test ===\r\n");
 
@@ -344,10 +339,10 @@ ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
       printf("Error: audio_frontend_init failed\r\n");
     }
     /* USER CODE END 5 */
-  }
+}
 
-  void MX_X_CUBE_AI_Process(void)
-  {
+void MX_X_CUBE_AI_Process(void)
+{
     /* USER CODE BEGIN 6 */
     int res = -1;
 
@@ -404,7 +399,7 @@ ai_i8 *data_outs[AI_MICRO_SPEECH_OUT_NUM] = {
 #endif
     }
     /* USER CODE END 6 */
-  }
+}
 #ifdef __cplusplus
 }
 #endif
--- X-CUBE-AI/App/micro_speech_data_params.c
+++ X-CUBE-AI/App/micro_speech_data_params.c
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    micro_speech_data_params.c
   * @author  AST Embedded Analytics Research Platform
-  * @date    2026-07-15T00:29:14+0800
+  * @date    2026-07-15T02:52:55+0800
   * @brief   AI Tool Automatic Code Generator for Embedded NN computing
   ******************************************************************************
   * Copyright (c) 2026 STMicroelectronics.
